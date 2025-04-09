ddc3ff36c3ebbe97224be5b1aa5eeb19bbe3c4ee
azolajokweni <144328836+azolajokweni@users.noreply.github.com>
2025-04-09 22:18:32 +0200

    Update Program.cs

--- CSharp Test/Program.cs
+++ CSharp Test/Program.cs
@@ -10,17 +10,21 @@ namespace CurrencyTempConverter
     {
         static void MyMethod()
         {
+            // Welcome message and user input
             Console.WriteLine("Greetings! Please enter the key 'c' to use the Currency Converter, or any other key to use the temperature converter:");
             string answer = Console.ReadLine();
 
             if (answer == "c")
             {
+                // If user inputs "c", then they are referred to the currency part of the app
+                // User can choose to convert from Rands to Dollars or visa versa
                 Console.WriteLine("Welcome to the Currency Converter.");
                 Console.WriteLine("Please enter the key 'r' to convert from Rands or any other key to convert from Dollars:");
                 string choice = Console.ReadLine();
 
                 if (choice == "r")
                 {
+                    // User input is read and parsed to a float, answer is given in Dollars
                     Console.WriteLine("Please enter your amount in Rands:");
                     float rands = float.Parse(Console.ReadLine());
                     float dollars = rands / 0.052f;
@@ -28,6 +32,7 @@ namespace CurrencyTempConverter
                 }
                 else
                 {
+                    // User input is read and parsed to a float, answer is given in Rands
                     Console.WriteLine("Please enter your amount in Dollars:");
                     float dollars = float.Parse(Console.ReadLine());
                     float rands = dollars * 19.38f;
@@ -36,12 +41,14 @@ namespace CurrencyTempConverter
             }
             else
             {
+                // Temperature part of the App
                 Console.WriteLine("Welcome to the Temperature Converter.");
                 Console.WriteLine("Please enter the key 't' to convert from Celsius or any other key to convert from Farenheit:");
                 string choice = Console.ReadLine();
 
                 if (choice == "t")
                 {
+                    // User input is read and parsed to a float, answer is given in Farenheit
                     Console.WriteLine("Enter a temperature in celsius:");
                     float celsius = float.Parse(Console.ReadLine());
                     float farenheit = celsius * 1.8f + 32f;
@@ -49,6 +56,7 @@ namespace CurrencyTempConverter
                 }
                 else
                 {
+                    // User input is read and parsed, answer is given in celsius
                     Console.WriteLine("Please enter a temperature in Farenheit:");
                     float farenheit = float.Parse(Console.ReadLine());
                     float celsius = (farenheit - 32f) / 1.8f;
