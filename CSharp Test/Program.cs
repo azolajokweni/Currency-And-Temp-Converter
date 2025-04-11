fa95b7fe1c42a78394222a92daa1cd393516b660
azolajokweni <144328836+azolajokweni@users.noreply.github.com>
2025-04-11 13:20:08 +0200

    Update Program.cs

--- CSharp Test/Program.cs
+++ CSharp Test/Program.cs
@@ -27,8 +27,8 @@ namespace CurrencyTempConverter
                     // User input is read and parsed to a float, answer is given in Dollars
                     Console.WriteLine("Please enter your amount in Rands:");
                     float rands = float.Parse(Console.ReadLine());
-                    float dollars = rands / 0.052f;
-                    Console.WriteLine("The amount in Dollars is: " + dollars.ToString("0.000"));
+                    float dollars = rands * 0.052f;
+                    Console.WriteLine("The amount in Dollars is: " + dollars.ToString("0.00"));
                 }
                 else
                 {
@@ -36,31 +36,31 @@ namespace CurrencyTempConverter
                     Console.WriteLine("Please enter your amount in Dollars:");
                     float dollars = float.Parse(Console.ReadLine());
                     float rands = dollars * 19.38f;
-                    Console.WriteLine("The amount in Rands is: " + rands.ToString("0.000"));
+                    Console.WriteLine("The amount in Rands is: " + rands.ToString("0.00"));
                 }
             }
             else
             {
                 // Temperature part of the App
                 Console.WriteLine("Welcome to the Temperature Converter.");
-                Console.WriteLine("Please enter the key 't' to convert from Celsius or any other key to convert from Farenheit:");
+                Console.WriteLine("Please enter the key 't' to convert from Celsius or any other key to convert from Fahrenheit:");
                 string choice = Console.ReadLine();
 
                 if (choice == "t")
                 {
-                    // User input is read and parsed to a float, answer is given in Farenheit
+                    // User input is read and parsed to a float, answer is given in Fahrenheit
                     Console.WriteLine("Enter a temperature in celsius:");
                     float celsius = float.Parse(Console.ReadLine());
-                    float farenheit = celsius * 1.8f + 32f;
-                    Console.WriteLine("The temperature in Farenheit is: " + farenheit);
+                    float fahrenheit = celsius * 1.8f + 32f;
+                    Console.WriteLine("The temperature in Fahrenheit is: " + fahrenheit.ToString("0.00"));
                 }
                 else
                 {
                     // User input is read and parsed, answer is given in celsius
-                    Console.WriteLine("Please enter a temperature in Farenheit:");
-                    float farenheit = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter a temperature in Fahrenheit:");
+                    float fahrenheit = float.Parse(Console.ReadLine());
                     float celsius = (farenheit - 32f) / 1.8f;
-                    Console.WriteLine("The temperature in celsius is: " + celsius);
+                    Console.WriteLine("The temperature in celsius is: " + celsius.ToString("0.00"));
                 }
             }
         }
